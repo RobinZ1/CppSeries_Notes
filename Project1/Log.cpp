ef38a0dc7fa80ac2e4f1fa65d17edd3d3466d5fd
Luobin-Zeng <q2721340562@gmail.com>
2019-07-22 15:50:06 +0800

    tut2 completed

--- Project1/Log.cpp
+++ Project1/Log.cpp
@@ -3,4 +3,4 @@
 void Log(const char* message)
 {
 	std::cout << message << std::endl;
-}
+}     
---
+++ Project1/Math.cpp
@@ -0,0 +1,10 @@
+const char* Log(const char* message)
+{
+	return message;
+}
+int Multipy(int a, int b)
+{
+	Log("Multiply");
+	return a*b;
+}
+
