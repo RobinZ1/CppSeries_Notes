6fcbf270e5b99267df089b524c88917f45802ea0
Luobin-Zeng <q2721340562@gmail.com>
2019-07-22 17:42:15 +0800

    tut 9 variables in cpp

--- Project1/Log.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-//inline void.... means to put function definition 
-//body and replace the call with it in cpp file.
-void Log(const char* message);
---
+++ Project1/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	//1b, 2b, 4b, 4b, 8b
+	//unsigned char, short, int, long, long long;
+
+	//float-4b, double -8b
+	float variable = 8.0f; //-2b -> 2b
+	float B = 8.0;
+	//the 8.0f means float, otherwise double
+	//bool - 1b
+	std::cout << variable << std::endl;
+	std::cin.get();
+}
