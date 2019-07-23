949085ddfaea24ffa84ebbe00c49e8f77ad8a196
Luobin-Zeng <q2721340562@gmail.com>
2019-07-23 15:26:06 +0800

    tut18 completed

--- Project1/src/main.cpp
+++ Project1/src/main.cpp
@@ -1,2 +1,17 @@
-#include <isotream>
+#include <iostream>
 
+#define LOG(x) std::cout<<x<<std::endl;
+
+void Increment(int& value)
+{
+	value++;
+}
+
+int main()
+{
+	int a = 5;
+	Increment(a);
+	LOG(a);
+
+	std::cin.get();
+}
