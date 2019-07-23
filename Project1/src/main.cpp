246c09c0715f5a74cdc1181922bfeabc3035f1c5
Luobin-Zeng <q2721340562@gmail.com>
2019-07-23 16:39:56 +0800

    tut 21 how to write c++ class

--- Project1/src/main.cpp
+++ Project1/src/main.cpp
@@ -1,2 +1,44 @@
 #include <iostream>
 
+class Log {
+public:
+	const int LogLevelError = 0;
+	const int LogLevelWarning = 1;
+	const int LogLevelInfo = 2;
+private:
+	int m_LogLevel = LogLevelInfo;
+public:
+	void SetLevel(int level)
+	{
+		m_LogLevel = level;
+	}
+
+	void Error(const char* message)
+	{
+		if(m_LogLevel >= LogLevelError)
+			std::cout << "[Error]: " << message << std::endl;
+	}
+
+	void Warn(const char* message)
+	{
+		if (m_LogLevel >= LogLevelWarning)
+			std::cout << "[Warning]: " << message << std::endl;
+	}
+
+	void Info(const char* message)
+	{
+		if (m_LogLevel >= LogLevelInfo)
+			std::cout << "[Info]: "<<message << std::endl;
+	}
+
+};
+
+int main()
+{
+	Log log;
+	log.Warn("Hello");
+	log.Error("Hello");
+	log.Info("Hello");
+	std::cin.get();
+
+}
