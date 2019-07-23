56f6b15c0ecf1942cf662d822f4d87ba74bc550f
Luobin-Zeng <q2721340562@gmail.com>
2019-07-23 16:24:20 +0800

    tut 21 class deep

--- Project1/src/main.cpp
+++ Project1/src/main.cpp
@@ -1,26 +1,2 @@
 #include <iostream>
 
-#define LOG(x) std::cout<<x<<std::endl;
-
-class Player {
-
-public:
-	int x, y;
-	int speed;
-
-public:
-	void Move(Player& player, int xa, int ya)
-	{
-		player.x += xa * player.speed;
-		player.y += ya * player.speed;
-	}
-};
-
-
-
-int main()
-{
-	Player player;
-	player.Move(player, 1, -1);
-	std::cin.get();
-}
