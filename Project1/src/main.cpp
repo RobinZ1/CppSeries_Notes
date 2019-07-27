ba52179b6081710b69ab0639c8f0b7488375bd1c
Luobin-Zeng <q2721340562@gmail.com>
2019-07-27 19:31:58 +0800

    TUT64 Timing in CPP

--- Project1/src/main.cpp
+++ Project1/src/main.cpp
@@ -1185,4 +1185,45 @@ int main()
 */
 
 
+
+
 //TUT64 TIMING
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+struct Timer
+{
+	std::chrono::time_point<std::chrono::steady_clock> start, end;
+	std::chrono::duration<float> duration;
+
+	Timer()
+	{
+		start = std::chrono::high_resolution_clock::now();
+		
+	}
+
+	~Timer()
+	{
+		end = std::chrono::high_resolution_clock::now();
+		duration = end - start;
+		float ms = duration.count()*(1000.0f);
+		std::cout << "Timer took " << ms << "ms " << std::endl;
+	};                                            
+};
+
+void Function()
+{
+	Timer timer;
+	for (int i = 0; i < 1000; i++)
+	{
+		std::cout << "Hello\n";
+	}
+}
+
+int main()
+{
+	Function();
+	std::cin.get();
+}
+
