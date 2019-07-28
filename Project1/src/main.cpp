8302fdf7875f660ebe46691936cd5886c745e364
Luobin-Zeng <q2721340562@gmail.com>
2019-07-28 13:01:40 +0800

    TUT66 Sorting in CPP

--- Project1/src/main.cpp
+++ Project1/src/main.cpp
@@ -1291,5 +1291,32 @@ int main()
 
 */
 
-
+/*
 //TUT 66 sorting in CPP
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+int main()
+{
+	std::vector<int> values = { 3,5,1,4,2 };
+	//std::sort(values.begin(), values.end(), std::greater<int>());
+	//or std::sort(values.begin(),values.end(), [](int a, int b) {return a>b}
+	std::sort(values.begin(), values.end(), [](int a, int b) {
+		//to follow 1 to the end of the list
+		//if a == 1, don't order it before b, move the back of b, 
+		//if b ==1. move a to the front of the list
+		if (a == 1) 
+			return false;
+		if (b == 1)
+			return true;
+		return a < b;
+		});//23451
+
+	for (int value : values)
+		std::cout << value << std::endl;
+
+	std::cin.get();
+}
+*/
